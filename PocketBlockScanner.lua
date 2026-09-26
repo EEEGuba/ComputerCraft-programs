47a4434e5f07abf648e0de81e73cf7c7862dd507
EEEGuba <126352589+EEEGuba@users.noreply.github.com>
2026-09-26 05:14:19 +0200

    Added PocketBlockScanner.lua quick dirty version

---
+++ PocketBlockScanner.lua
@@ -0,0 +1,27 @@
+local geo = peripheral.wrap("back")
+local sizeX, sizeY = term.getSize()
+ 
+while true do
+    local scan = geo.scan(16)
+    term.clear()
+    term.setTextColor(colors.blue)
+    term.setCursorPos(sizeX/2,sizeY/2)
+    term.write("X")
+    for i, block_data in ipairs(scan) do
+         if block_data.name == "minecraft:stone_bricks" or block_data.name == "minecraft:chest" then       
+             term.setTextColor(colors.red)
+             term.setCursorPos(sizeX/2-block_data.x/1.4,sizeY/2-block_data.z/1.8)
+             if block_data.y>1 then
+                 term.write("0")
+             else 
+                 if block_data.y<-1 then
+                     term.write("o")
+                 else 
+                     term.write("x")
+                 end
+             end
+             term.setTextColor(colors.white)
+         end
+    end
+end
+ 
